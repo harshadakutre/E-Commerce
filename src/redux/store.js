68923ad0f97b2b788b34547aff7ad5a9f434da98
harshadakutre <140054640+harshadakutre@users.noreply.github.com>
2023-12-21 13:21:57 +0530

    updated store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import SignUpSlice from "../components/LoginSlice.js";
+import cartSlice from "../components/cartSlice.js";
 
 const store = configureStore({
   reducer: {
     signUpReducer: SignUpSlice,
+    cartReducer: cartSlice,
   },
 });
 
